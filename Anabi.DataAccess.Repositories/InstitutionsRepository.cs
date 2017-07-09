c2931063c59beabb8a070017a5760cf924b4d209
bioan <85bioan@gmail.com>
2017-07-09 15:03:05 +0300

    Added Institutions

---
+++ Anabi.DataAccess.Repositories/InstitutionsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Anabi.DataAccess.Ef;
+using Anabi.DataAccess.Ef.DbModels;
+using Anabi.DataAccess.Abstractions.Repositories;
+
+namespace Anabi.DataAccess.Repositories
+{
+    class InstitutionsRepository : GenericRepository<InstitutionDb>
+    {
+        public InstitutionsRepository(AnabiContext ctx) : base(ctx)
+        {
+
+        }
+
+    }
+}
