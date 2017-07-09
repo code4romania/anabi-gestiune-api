c785321b2f559d1eec5bf4fdeae0a9e344726c96
bioan <85bioan@gmail.com>
2017-07-09 15:02:52 +0300

    Added Counties

---
+++ Anabi.DataAccess.Repositories/CountiesRepository.cs
@@ -0,0 +1,15 @@
+﻿using Anabi.DataAccess.Ef;
+using Anabi.DataAccess.Ef.DbModels;
+using Anabi.DataAccess.Abstractions.Repositories;
+
+namespace Anabi.DataAccess.Repositories
+{
+    class CountiesRepository : GenericRepository<CountyDb>
+    {
+        public CountiesRepository(AnabiContext ctx) : base(ctx)
+        {
+
+        }
+
+    }
+}
