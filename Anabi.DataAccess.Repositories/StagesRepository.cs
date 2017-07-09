758c9779087e5998513ef79298d3371dde773ffe
bioan <85bioan@gmail.com>
2017-07-09 15:04:34 +0300

    Added Stages

---
+++ Anabi.DataAccess.Repositories/StagesRepository.cs
@@ -0,0 +1,15 @@
+﻿using Anabi.DataAccess.Ef;
+using Anabi.DataAccess.Ef.DbModels;
+using Anabi.DataAccess.Abstractions.Repositories;
+
+namespace Anabi.DataAccess.Repositories
+{
+    class StagesRepository : GenericRepository<StageDb>
+    {
+        public StagesRepository(AnabiContext ctx) : base(ctx)
+        {
+
+        }
+
+    }
+}
