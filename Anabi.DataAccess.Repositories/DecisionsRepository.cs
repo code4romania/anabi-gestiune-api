4b57efc8be4e3eb977e917d3f730bd56b22fbe1b
bioan <85bioan@gmail.com>
2017-07-18 16:13:00 +0300

    ANABI-43 Added Decisions controller + repo

---
+++ Anabi.DataAccess.Repositories/DecisionsRepository.cs
@@ -0,0 +1,14 @@
+﻿using Anabi.DataAccess.Ef;
+using Anabi.DataAccess.Ef.DbModels;
+using Anabi.DataAccess.Abstractions.Repositories;
+
+namespace Anabi.DataAccess.Repositories
+{
+    public class DecisionsRepository : GenericRepository<DecisionDb>
+    {
+        public DecisionsRepository(AnabiContext ctx) : base(ctx)
+        {
+
+        }
+    }
+}
