763effcc166a5d26affc7a664085b1c783194cd5
alex.albu@outlook.com <alex.albu@outlook.com>
2017-11-17 22:38:07 +0100

    Anabi-86 Search Assets

---
+++ Anabi/Features/Assets/Models/SearchAsset.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anabi.Features.Assets.Models
+{
+    public class SearchAsset : IRequest<List<AssetSummary>>
+    {
+        public string DecisionNumber { get; set; }
+        public string FileNumber { get; set; }
+        // Identification number
+        public string PersonID { get; set; }
+        public string PersonName { get; set; }
+        public string AssetDescriptionOrIdentifier { get; set; }
+    }
+    
+}
---
+++ Anabi/Features/Assets/SearchAssetHandler.cs
@@ -0,0 +1,68 @@
+﻿using Anabi.Domain;
+using Anabi.Features.Assets.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Anabi.DataAccess.Ef;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anabi.Features.Assets
+{
+    public class SearchAssetHandler : BaseHandler, IAsyncRequestHandler<SearchAsset, List<AssetSummary>>
+    {
+        public SearchAssetHandler(AnabiContext _ctx, IMapper _mapper) : base(_ctx, _mapper)
+        {
+        }
+
+        public async Task<List<AssetSummary>> Handle(SearchAsset message)
+        {
+
+            var query = from asset in context.Bunuri
+                        join cat in context.Categorii on asset.CategoryId equals cat.Id
+                        join assetFile in context.BunuriDosare on asset.Id equals assetFile.AssetId
+                        join file in context.Dosare on assetFile.FileId equals file.Id
+                        join fileNumber in context.NumereDosare on file.CurrentFileNumber equals fileNumber.FileNumber
+                        join defendant in context.InculpatiDosar on file.Id equals defendant.FileId
+                        join person in context.Persoane on defendant.PersonId equals person.Id
+
+                        from historicalStage in context.EtapeIstorice
+                                                .Where(x => x.AssetId == asset.Id)
+                                                .OrderByDescending(x => x.DecisionDate)
+                                                .Take(1) 
+                            join institution in context.Institutii on historicalStage.InstitutionId equals institution.Id
+                            join decision in context.Decizii on historicalStage.DecizieId equals decision.Id
+                            join stage in context.Etape on historicalStage.StageId equals stage.Id
+
+                        where (string.IsNullOrEmpty(message.PersonID) || person.Identification == message.PersonID)
+                        where (string.IsNullOrEmpty(message.PersonName) || person.Name == message.PersonName)
+                        select new AssetSummary
+                        {
+                            FileNumber = file.InitialFileNumber,
+                            AssetCategory = cat.Code,
+                            AssetDescription = asset.Description,
+                            AssetId = asset.Id,
+                            AssetIdentifier = asset.Identifier,
+                            Institution = institution.Name,
+                            CurrentDecision = decision.Name,
+                            CurrentStage = stage.Name
+                            
+                        };
+
+            if (!string.IsNullOrEmpty(message.DecisionNumber))
+            {
+                query = query.Where(q => q.CurrentDecision == message.DecisionNumber);
+            }
+
+            if (!string.IsNullOrEmpty(message.FileNumber))
+            {
+                query = query.Where(q => q.FileNumber == message.FileNumber);
+            }
+
+            return await query.ToListAsync();
+
+        }
+    }
+}
