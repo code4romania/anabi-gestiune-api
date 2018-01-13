321986ad333a5189ca14770a70e0ed5ddb600a86
Bogdan <bogdan.vizureanu@gmail.com>
2018-01-13 19:01:14 +0200

    ANABI-104 - GET /api/Counties

---
+++ Anabi/Features/Counties/CountiesController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Anabi.Controllers;
+using Anabi.Domain.Models;
+using Anabi.Features.Counties.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anabi.Features.Counties
+{
+    [Produces("application/json")]
+    [Route("api/Counties")]
+    public class CountiesController : BaseController
+    {
+        private readonly IMediator _mediator;
+
+        public CountiesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        //GET /api/Counties
+        [HttpGet]
+        public async Task<IEnumerable<County>> Get()
+        {
+            var models = await _mediator.Send(new GetCounties());
+            return models;
+        }
+
+    }
+}
