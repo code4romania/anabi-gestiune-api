8e972019bc472a97b15cab2d820bb53bd722956b
Alex Albu <alex.albu@acando.no>
2018-02-05 22:06:48 +0100

    ANABI-113 Model for Post endpoint

---
+++ Anabi.Domain.Core/Asset/Commands/AddMinimalAsset.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anabi.Domain.Asset.Commands
+{
+    public class AddMinimalAsset : IRequest<int>
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string Identifier { get; set; }
+
+        public string UserCode { get; set; }
+
+        public int StageId { get; set; }
+    }
+}
