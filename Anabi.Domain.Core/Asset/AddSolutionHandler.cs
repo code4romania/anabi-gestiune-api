719bad6d9b2326c3890a463a8cf742cf07f27355
alex.albu@acando.no <alexp.albu@gmail.com>
2018-05-19 12:17:15 +0200

    Handler

--- Anabi.Domain.Core/Asset/AddSolutionHandler.cs
+++ Anabi.Domain.Core/Asset/AddSolutionHandler.cs
@@ -1,8 +1,6 @@
-﻿using Anabi.Domain.Asset.Commands;
+﻿using Anabi.DataAccess.Ef.DbModels;
+using Anabi.Domain.Asset.Commands;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Anabi.Domain.Asset
@@ -16,7 +14,50 @@ namespace Anabi.Domain.Asset
 
         public async Task<int> Handle(AddSolution message)
         {
-            throw new NotImplementedException();
+
+            var newStage = new HistoricalStageDb
+            {
+                AssetId = message.AssetId,
+                StageId = message.StageId,
+                DecizieId = message.DecisionId,
+                InstitutionId = message.InstitutionId,
+                DecisionDate = message.DecisionDate,
+                DecisionNumber = message.DecisionNumber,
+                RecoveryBeneficiaryId = message.RecoveryBeneficiaryId,
+            };
+            
+            newStage.ActualValue = message.RecoveryDetails?.ActualValue;
+            newStage.EstimatedAmount = message.RecoveryDetails?.EstimatedAmount;
+            newStage.EstimatedAmountCurrency = message.RecoveryDetails?.EstimatedAmountCurrency;
+            newStage.ActualValueCurrency = message.RecoveryDetails?.ActualValueCurrency;
+            newStage.RecoveryState = message.RecoveryDetails?.RecoveryState;
+
+            newStage.EvaluationCommittee = message.RecoveryDetails?.EvaluationCommittee?.EvaluationCommitteeMembers;
+            newStage.EvaluationCommitteePresident = message.RecoveryDetails?.EvaluationCommittee?.EvaluationCommitteePresident;
+            newStage.EvaluationCommitteeDesignationDate = message.RecoveryDetails?.EvaluationCommittee?.EvaluationCommitteeDesignationDate;
+
+            newStage.RecoveryCommittee = message.RecoveryDetails?.RecoveryCommittee?.RecoveryCommitteeMembers;
+            newStage.RecoveryCommitteePresident = message.RecoveryDetails?.RecoveryCommittee?.RecoveryCommitteePresident;
+            newStage.RecoveryCommitteeDesignationDate = message.RecoveryDetails?.RecoveryCommittee?.RecoveryCommitteeDesignationDate;
+
+            newStage.LastActivity = message.RecoveryDetails?.LastActivity;
+            newStage.PersonResponsible = message.RecoveryDetails?.PersonResponsible;
+
+            newStage.Source = message.SolutionDetails?.Source;
+            newStage.SentOnEmail = message.SolutionDetails?.SentOnEmail;
+            newStage.FileNumber = message.SolutionDetails?.FileNumber;
+            newStage.FileNumberParquet = message.SolutionDetails?.FileNumberParquet;
+            newStage.ReceivingDate = message.SolutionDetails?.ReceivingDate;
+            newStage.IsDefinitive = message.SolutionDetails?.IsDefinitive;
+            newStage.DefinitiveDate = message.SolutionDetails?.DefinitiveDate;
+            newStage.SendToAuthoritiesDate = message.SolutionDetails?.SentToAuthoritiesDate;
+            newStage.CrimeTypeId = message.SolutionDetails?.CrimeTypeId;
+            newStage.LegalBasis = message.SolutionDetails?.LegalBasis;
+
+            context.HistoricalStages.Add(newStage);
+            await context.SaveChangesAsync();
+
+            return newStage.Id;
         }
     }
 }
