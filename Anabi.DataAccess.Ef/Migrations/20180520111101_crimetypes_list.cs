b105c3409739178b1963dbda810e4ad409df5773
alex.albu@acando.no <alexp.albu@gmail.com>
2018-05-20 13:16:42 +0200

    Crime types endpoint

---
+++ Anabi.DataAccess.Ef/Migrations/20180520111101_crimetypes_list.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Anabi.DataAccess.Ef.Migrations
+{
+    public partial class crimetypes_list : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert CrimeTypes(crimename, usercodeadd, addeddate) values('Furt calificat', 'admin', getdate())");
+            migrationBuilder.Sql("insert CrimeTypes(crimename, usercodeadd, addeddate) values('Spalare de bani', 'admin', getdate())");
+            migrationBuilder.Sql("insert CrimeTypes(crimename, usercodeadd, addeddate) values('Coruptie', 'admin', getdate())");
+            migrationBuilder.Sql("insert CrimeTypes(crimename, usercodeadd, addeddate) values('Altele', 'admin', getdate())");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
