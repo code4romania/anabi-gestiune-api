7a4bd494cc962ed3eac11acbc6c7c0ba9325be68
Andrei Mustata <a.mustata@levi9.com>
2019-03-30 16:58:21 +0200

    Add Institutions.ContactData migrations

--- Anabi.DataAccess.Ef/Migrations/20190330145738_AddInstitutionContactData.Designer.cs
+++ Anabi.DataAccess.Ef/Migrations/20190330145738_AddInstitutionContactData.Designer.cs
@@ -4,14 +4,16 @@ using Anabi.DataAccess.Ef;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Anabi.DataAccess.Ef.Migrations
 {
     [DbContext(typeof(AnabiContext))]
-    partial class AnabiContextModelSnapshot : ModelSnapshot
+    [Migration("20190330145738_AddInstitutionContactData")]
+    partial class AddInstitutionContactData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
