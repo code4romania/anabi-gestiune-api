118479889cc9f683c668be7d184f912541c1c405
noalealb <alex.albu@acando.no>
2019-04-02 22:43:19 +0200

    Db entity and start on Integration test

---
+++ Anabi.Integration.Tests/Assets/AddSolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Anabi.Integration.Tests.Assets
+{
+    public class AddSolutionTests : ApiTests
+    {
+        public AddSolutionTests(AnabiApplicationFactory<Startup> factory) : base(factory)
+        {
+         
+        }
+
+        [Fact]
+        public async Task AddSolution_HasRecoveryFields()
+        {
+            
+        }
+    }
+}
