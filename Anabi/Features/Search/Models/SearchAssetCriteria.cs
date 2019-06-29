e679c33686f4b4cb8719da1a9007108003ee8d25
Cosmin Popescu <cosmin.popescu93@gmail.com>
2019-06-29 11:42:59 +0300

    https://github.com/code4romania/anabi-gestiune-api/issues/104

---
+++ Anabi/Features/Search/Models/SearchAssetCriteria.cs
@@ -0,0 +1,18 @@
+﻿using Anabi.Features.Assets.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anabi.Features.Search.Models
+{
+    public class SearchAssetCriteria : IRequest<List<SearchAssetResult>>
+    {
+        public string DecisionNumber { get; set; }
+        public string FileNumber { get; set; }
+        public string DefendantId { get; set; } //numeric personal code or fiscal unique registrain code
+        public string DefendantName { get; set; }
+
+    }
+}
---
+++ Anabi/Features/Search/SearchAssetHandler.cs
@@ -0,0 +1,47 @@
+﻿using Anabi.Domain;
+using Anabi.Features.Assets.Models;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using Anabi.Features.Search.Models;
+
+namespace Anabi.Features.Search
+{
+    public class SearchAssetHandler : BaseHandler, IRequestHandler<SearchAssetCriteria, List<SearchAssetResult>>
+    {
+        public SearchAssetHandler(BaseHandlerNeeds needs) : base(needs)
+        {
+        }
+
+        /**
+         * Method that returns a search result  
+         * */
+        public async Task<List<SearchAssetResult>> Handle(SearchAssetCriteria message, CancellationToken cancellationToken)
+        {
+            var query = from historicalStages in context.HistoricalStages
+                        join assets in context.Assets on historicalStages.AssetId equals assets.Id
+                        join assetDefendant in context.AssetDefendants on assets.Id equals assetDefendant.AssetId
+                        join person in context.Persons on assetDefendant.PersonId equals person.Id
+                        join stages in context.Stages on historicalStages.StageId equals stages.Id
+
+                        where historicalStages.DecisionNumber == message.DecisionNumber
+                        where historicalStages.FileNumber == message.FileNumber
+                        where historicalStages.Person.Identification == message.DefendantId
+                        where historicalStages.Person.Name == message.DefendantName
+                        select new SearchAssetResult
+                        {
+                            //Id = assets.Id,
+                            DefendantName = person.Name,
+                            DecisionNumber = historicalStages.DecisionNumber,
+                            FileNumber = historicalStages.FileNumber,
+                            Stage = historicalStages.Stage.Name,
+                            AssetId = assets.Id
+                        };
+                                                                      
+            return await query.ToListAsync(cancellationToken);
+        }
+    }
+}
