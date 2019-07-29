eac8be2fef330498d36ed045c8ec77d2aeff845a
Botez Ioan <85bioan@gmail.com>
2019-07-29 14:56:27 +0300

    Added Owner relation to DB

---
+++ Anabi.DataAccess.Ef/EntityConfigurators/AssetOwnerConfig.cs
@@ -0,0 +1,29 @@
+﻿using Anabi.DataAccess.Ef.DbModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Anabi.DataAccess.Ef.EntityConfigurators
+{
+    public class AssetOwnerConfig : BaseEntityConfig<AssetOwnerDb>
+    {
+
+        public override void Configure(EntityTypeBuilder<AssetOwnerDb> builder)
+        {
+            builder.HasOne(a => a.Asset)
+                .WithMany(a => a.Owners)
+                .HasForeignKey(k => k.AssetId)
+                .OnDelete(DeleteBehavior.Restrict)
+                .HasConstraintName("FK_Assets_AssetOwner")
+                .IsRequired();
+
+            builder.HasOne(a => a.Owner)
+                .WithMany(a => a.Owners)
+                .HasForeignKey(k => k.PersonId)
+                .OnDelete(DeleteBehavior.Restrict)
+                .HasConstraintName("FK_Person_AssetOwner")
+                .IsRequired();
+
+            base.Configure(builder);
+        }
+    }
+}
